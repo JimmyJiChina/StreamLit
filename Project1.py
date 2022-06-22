83d13fad6adc6ba90f15d9431ec4a46bc775bc5d
jimmyBRCMChina <5142973+jimmybrcmchina@user.noreply.gitee.com>
2022-06-22 23:43:41 +0800

    first commit

---
+++ Project1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+xls_file = st.file_uploader('uploading excel file')
+if xls_file:
+    print('data uploaded')
+    st.write("data uploaded")
+
+
