b9ad55b08db1cd3624fef296ece9dc8ec242d335
jimmyBRCMChina <5142973+jimmybrcmchina@user.noreply.gitee.com>
2022-06-23 00:41:35 +0800

    typo tile to title

--- Project1.py
+++ Project1.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 
 xls_file = st.file_uploader('uploading excel file')
-st.tile("This is Jimmy Ji Play Ground")
+st.title("This is Jimmy Ji Play Ground")
 if xls_file:
     print('data uploaded')
     st.write("data uploaded")
